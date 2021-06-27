74ae78948231c9556b218dd345c2e6af79eceedb
Abrar Hayat <abrarhayat@gmail.com>
2021-06-28 01:03:18 +0600

    Added a Health Component to track Actors' Health

---
+++ Source/ToonTanks/Components/HealthComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class ATankGameModeBase;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+	UPROPERTY(EditAnywhere)
+	float DefaultHealth = 100.0f;
+
+	float Health = 0.0f;
+
+	ATankGameModeBase * GameModeRef;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void TakeDamage(AActor *DamagedActor, float DamageAmount, const UDamageType *DamageType, AController *InstigatedBy, AActor *DamageCauser);
+};
