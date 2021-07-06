a92e7b766441a4c7f31da28435e32fdfe1493d75
Abrar Hayat <abrarhayat@gmail.com>
2021-07-07 00:19:43 +0600

    Improved the Start Menu to pause the game until we click start game and also we are always showing the cursor for the menu

--- Source/ToonTanks/PlayerControllers/PlayerControllerBase.cpp
+++ Source/ToonTanks/PlayerControllers/PlayerControllerBase.cpp
@@ -12,5 +12,5 @@ void APlayerControllerBase::SetPlayerEnabledState(bool PlayerEnabledState)
     {
         GetPawn()->DisableInput(this);
     }
-    bShowMouseCursor = PlayerEnabledState;
+    bShowMouseCursor = true;
 }
